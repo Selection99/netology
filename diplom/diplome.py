1cee25238c85ad8e9da670a5b6878d8aef4483c2
Selection66 <selection66@yandex.ru>
2018-08-22 22:38:50 +0300

    Fixes

--- diplom/diplome.py
+++ diplom/diplome.py
@@ -8,7 +8,7 @@ SLEEP_TIME = 0.8
 # token = '7b23e40ad10e08d3b7a8ec0956f2c57910c455e886b480b7d9fb59859870658c4a0b8fdc4dd494db19099'
 # target_id = 171691064
 
-token = '19d7aea6bb8cdd5c50bd35a1c7588d3db11adf1135f80cd218e9a103d518ce9337ea9bf438e82047e1a28'
+token = '6111587315a08f76f5820f4084cdeb1cb97666a87e999b2800e23af16aef0151c957b11d479ac2590f075'
 target_id = 266645095
 # target_id = 26549682
 
@@ -37,9 +37,11 @@ class VkApi:
 API = VkApi()
 
 
-def ask_till_answer(default_answer, max_tries=10):
+def ask_till_answer(default_answer, max_tries=6, start_msg=''):
     def decorator(func):
         def wrapped(*args, **kwargs):
+            if start_msg:
+                print(start_msg, end='')
             for i in range(max_tries):
                 try:
                     result = func(*args, **kwargs)
@@ -52,16 +54,14 @@ def ask_till_answer(default_answer, max_tries=10):
                         sleep(SLEEP_TIME)
                     else:
                         print('', response.json()['error']['error_msg'])
-                        print(response.text)
                         return default_answer
             raise RuntimeError("\nПревышено количество запросов")
         return wrapped
     return decorator
 
 
-@ask_till_answer(list())
+@ask_till_answer(list(), start_msg='Получение списка друзей цели')
 def friends():
-    print('Получение списка друзей цели:', end='')
     global response
     response = API.friends.get(
         user_id=target_id,
@@ -82,21 +82,14 @@ def groups(user_id):
 
 def make_set_of_groups(people_list):
     data = set()
-    for i, person in enumerate(people_list):
-        print(f'{len(people_list) - i}.', end='')
-        data = data.union(groups(person))
-        # sleep(SLEEP_TIME)
-    return groups(target_id) - data
-
-
-@ask_till_answer(list())
-def get_groups_by_id(set_of_groups):
-    print('Group research', end='')
-    global response
-    response = API.groups.getById(
-        group_ids=','.join([str(group_id) for group_id in set_of_groups]),
-    )
-    return response.json()['response']
+    print('Получение списка групп цели', end='')
+    target_groups = groups(target_id)
+    if people_list:
+        print('Получение списка групп каждого друга:')
+        for i, person in enumerate(people_list):
+            print(f'  {len(people_list) - i}.', end='')
+            data = data.union(groups(person))
+    return target_groups - data
 
 
 @ask_till_answer(0)
@@ -109,23 +102,38 @@ def get_count_of_group_members(group_id):
     return response.json()['response']['count']
 
 
-def get_json_groups(file, group_print_list):
-    data = []
-    print("Сбор информации о найденных группах:")
-    for i, group in enumerate(group_print_list):
-        print(f"{len(group_print_list)-i}.", end='')
-        data.append(dict(
-                name=group['name'],
-                gid=group['screen_name'],
-                members_count=get_count_of_group_members(group['id'])
-            )
+def get_json_groups(file, set_of_groups):
+    @ask_till_answer(list())
+    def get_groups_by_id():
+        global response
+        response = API.groups.getById(
+            group_ids=','.join([str(group_id) for group_id in set_of_groups]),
         )
-    pprint(data)
+        return response.json()['response']
+
+    data = []
+    if set_of_groups:
+        print("Сбор информации о найденных группах:")
+        print(' Получение глобальной информации о группах', end='')
+        group_print_list = get_groups_by_id()
+        print(" Получение подробной информации о каждой группе:")
+        for i, group in enumerate(group_print_list):
+            print(f"  {len(group_print_list)-i}.", end='')
+            data.append(dict(
+                    name=group['name'],
+                    gid=group['screen_name'],
+                    members_count=get_count_of_group_members(group['id'])
+                )
+            )
+        print('Результат:')
+        pprint(data)
+    else:
+        print('Список групп пуст.')
     with open(file, mode='w', encoding='utf-8') as f:
         f.write((dumps(data, ensure_ascii=False, indent='    ')))
 
 
 try:
-    get_json_groups('res.txt', get_groups_by_id(make_set_of_groups(friends())))
+    get_json_groups('res.txt', make_set_of_groups(friends()))
 except RuntimeError as err:
     print(*err.args)
--- hw_3.2/api_vk.py
+++ hw_3.2/api_vk.py
@@ -3,7 +3,7 @@ from requests import get
 https://oauth.vk.com/authorize?client_id=6652484&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.8
 """
 
-token = '1944478ab1bd52aa85c4ddeed423f1133be989e75c05fd6b64d50104f25ff83d854deabb7a006fb7db4dd'
+token = '6111587315a08f76f5820f4084cdeb1cb97666a87e999b2800e23af16aef0151c957b11d479ac2590f075'
 v = 5.80
 app_id = 6652484
 id_1 = 26549682
